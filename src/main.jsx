c28209924b3898a4c64a4c36adca4440ce8e5636
hokage-deepanshu <149322663+hokage-deepanshu@users.noreply.github.com>
2025-07-21 16:40:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Global CSS yahan import hota hai
+import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';  // ✅ ADD THIS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>       {/* ✅ Wrap your App here */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
